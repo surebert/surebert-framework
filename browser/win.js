ce470660579c8e464ecf717c50737f4c60fc5d5c
visco <visco@874840ac-8b26-453e-a097-40cf495e22eb>
2008-09-23 04:54:14 +0000

    removing sb2 branch as it is no longer a branch but has been merged into the trunk

--- browser/win.js
+++
@@ -1,6 +0,0 @@
-sb.browser.win = function(href, width, height, name){
-	name = name || 'docwin';
-	var w = window.open(href, name,"width="+width+",height="+height+",toolbar=0,scrollbar=1,resizable=1");
-	w.focus();
-	return w;
-};
