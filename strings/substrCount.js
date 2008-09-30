c9c77dfeba02750c5933e9eab38f369b57728869
visco <visco@874840ac-8b26-453e-a097-40cf495e22eb>
2008-09-30 16:03:25 +0000

    updated sb.strings.substrCount which no longer uses regex.  You can still grab the old version from one of the old tags but really could just use regex if wanted

--- strings/substrCount.js
+++ strings/substrCount.js
@@ -1,10 +1,9 @@
 /**
 @Name: sb.strings.substrCount
 @Author: Paul Visco
-@Version: 1.0 11/19/07
+@Version: 1.1 11/19/07 09/18/08
 @Description: Returns the numbers of times a substring is found in a string
 @Param: String needle The substring to search for within the string
-@Param: Boolean caseInsensitive Optional - If true the search is case insensitive
 @Return: Number The number of times the substring is found in the original string
 @Example:
 var myString = 'hello world on earth';
@@ -12,22 +11,16 @@ var myString = 'hello world on earth';
 var answer = myString.substrCount('world');
 //answer = 1;
 
-var answer = myString.substrCount('World', true);
-//answer = 1;
-
 //or
 sb.strings.substrCount.call(myString);
 */
-sb.strings.substrCount = function(needle, caseInsensitive){
-	var matches, ig = (caseInsensitive === undefined) ? 'g' : 'ig';
-		
-	var re = new RegExp(needle, ig);
-	matches = this.match(re);
-	if(matches !==null){
-		return matches.length;
-	} else {
-	 	return false;
-	}
+sb.strings.substrCount = function(needle){
+	var cnt = 0;
+	for (var i=0;i<this.length;i++) {
+	if (needle == this.substr(i,needle.length))
+		cnt++;
+	} 
+	return cnt;
 };
 
 String.prototype.substrCount = sb.strings.substrCount;
