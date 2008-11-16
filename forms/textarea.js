4af4abb78f5c5eea3236d6b21cf2123f0200d8d6
visco <visco@874840ac-8b26-453e-a097-40cf495e22eb>
2008-11-16 18:54:48 +0000

    moved textarea.allowTabs into its own file

--- forms/textarea.js
+++ forms/textarea.js
@@ -168,33 +168,5 @@ sb.forms.textarea = {
 			range.select();
 		}
 		field.focus();
-	},
-
-	/**
-	@Name: sb.forms.textarea.allowTabs
-	@Description: Allows the user to use tabs in a textarea
-	@Param: String textarea The id or a reference to the textarea which you want to allow tabs on.
-
-	@Example:
-		s$(''#myTextArea').event('keydown', sb.forms.textarea.allowTabs);
-	*/
-	allowTabs : function(e){
-		var sel = sb.forms.textarea.getSelection(this), textarea = this, scrollTo;
-	
-		if(e.keyCode == 9){
-		
-				scrollTo = this.scrollTop;
-				window.setTimeout(function(){
-					textarea.value = sel.before + unescape('%09') + sel.after;	
-					sb.forms.textarea.moveCaret(textarea, sel.end+1);
-					textarea.focus();
-				}, 0);
-				setTimeout(function(){
-					textarea.scrollTop =  scrollTo;
-				}, 10);
-		
-			return false;
-		}
 	}
-	
 };
---
+++ forms/textarea/allowTabs.js
@@ -0,0 +1,28 @@
+/**
+@Name: sb.forms.textarea.allowTabs
+@Description: Allows the user to use tabs in a textarea
+@Param: String textarea The id or a reference to the textarea which you want to allow tabs on.
+
+@Example:
+	s$('#myTextArea').events({
+		keydown :  sb.forms.textarea.allowTabs
+	});
+*/
+sb.forms.textarea.allowTabs = function(e){
+	var sel = sb.forms.textarea.getSelection(this), textarea = this, scrollTo;
+
+	if(e.keyCode == 9){
+	
+			scrollTo = this.scrollTop;
+			window.setTimeout(function(){
+				textarea.value = sel.before + unescape('%09') + sel.after;	
+				sb.forms.textarea.moveCaret(textarea, sel.end+1);
+				textarea.focus();
+			}, 0);
+			setTimeout(function(){
+				textarea.scrollTop =  scrollTo;
+			}, 10);
+	
+		return false;
+	}
+};
