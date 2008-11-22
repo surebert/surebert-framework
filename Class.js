67e339c4bf74fb993a51d698ca74a2b00444fe77
visco <visco@874840ac-8b26-453e-a097-40cf495e22eb>
2008-11-22 07:42:34 +0000

    added support for calling parent.init() init from subclass init()

--- Class.js
+++ Class.js
@@ -31,55 +31,60 @@ var Employee = Person.extend({
 */
 sb.Class = function(o){
 	
-		var c = function(){
-			
-			if(typeof o.init == 'function'){
-				o.init.apply(this, arguments);
+	var c = function(){
 		
-			}
+		if(typeof o.init == 'function'){
 			
-		};
-		
-		c.prototype.typeOf = function(){
-			return o.type;
+			o.init.apply(this, arguments);
+	
 		}
+		
+	};
+	
+	c.prototype.typeOf = function(){
+		return o.type;
+	}
 
-		for(var prop in o){
-			c.prototype[prop] = o[prop];
+	for(var prop in o){
+		c.prototype[prop] = o[prop];
+	}
+	//c.prototype.init = o.init;
+	c.type = o.type || 'Class';
+	
+	c.extend = function(o2){
+		
+		for(prop in c.prototype){
+			
+			if(!o2[prop]){
+				o2[prop] = c.prototype[prop];
+			} 
+		}
+		 
+		var n = sb.Class(o2);
+		
+		n.prototype.isa = function(t){
+			var types = [this.type];
+			var parent = this.parent;
+			do{
+				types.push(parent.type);
+			} while (parent = parent.parent);
+		
+			return types.inArray(t);
+			
 		}
 		
-		c.type = o.type || 'Class';
-		
-		c.extend = function(o){
+		for(var prop in o2){
+			n.prototype[prop] = o2[prop];
+		}
+		n.prototype.parent = this.prototype;
+	
+		n.prototype.parent.init = function(){
 			
-			for(prop in c.prototype){
-				
-				if(!o[prop]){
-					o[prop] = c.prototype[prop];
-				}
-			}
-			
-			var n = sb.Class(o);
-			
-			n.prototype.isa = function(t){
-				var types = [this.type];
-				var parent = this.parent;
-				do{
-					types.push(parent.type);
-				} while (parent = parent.parent);
-			
-				return types.inArray(t);
-				
-			}
-			
-			for(var prop in o){
-				n.prototype[prop] = o[prop];
-			}
-			
-			n.prototype.parent = this.prototype;
-			return n;
+			return o.init.apply(n.prototype, arguments);
 		};
 		
-		
-		return c;
+		return n;
+	};
+	
+	return c;
 };
