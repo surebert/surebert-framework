a3a848d32338977f9edbf1164e4357e9faf28c6f
visco <visco@874840ac-8b26-453e-a097-40cf495e22eb>
2008-11-22 17:44:57 +0000

    minor tweeks to make parent init work

--- Class.js
+++ Class.js
@@ -5,12 +5,11 @@
 @Description: Expirimental - do not use for produciton code yet - Used to create extensible classes
 @Example:
 sb.include('Class');
-
 var Person = sb.Class({
-	init : function(){
-		this.name = 'jimmy';
+	init : function(name){
+		this.name = name;
 	},
-	type : 'person',
+	type : 'Person',
 	say : function(){
 		sb.consol.log('I am an person');
 	}
@@ -18,26 +17,50 @@ var Person = sb.Class({
 
 var Employee = Person.extend({
 	init : function(name, id){
+	
 		this.name = name;
+		//this.Person.init.call(this, name, id);
 		this.id = id;
 	},
-	type : 'employee',
+	type : 'Employee',
 	say : function(){
+		
 		this.parent.say();
 		sb.consol.log('I am an employee');
 	}
 });
 
+var Manager = Employee.extend({
+	init : function(name, id, super_id){
+		this.Employee.init.call(this, name, id);
+		//this.Employee.init.call(this, name, id);
+		this.super_id = super_id;
+	},
+	type : 'Manager',
+	say : function(){
+		this.parent.say();
+		sb.consol.log('I am a manager');
+	}
+});
+
+
+var y = new Manager('paul', 14650, 3009);
+
+//alert(y.getParents());
+y.say();
+sb.consol.dump(y);
+
 */
 sb.Class = function(o){
 	
-	var c = function(){
-		
+	var c = function(p){
+	alert(p);
+		//this.parent.init = o.init;
 		if(typeof o.init == 'function'){
-			
 			o.init.apply(this, arguments);
-	
 		}
+
+		
 		
 	};
 	
@@ -50,41 +73,92 @@ sb.Class = function(o){
 	}
 	//c.prototype.init = o.init;
 	c.type = o.type || 'Class';
+
+	
 	
 	c.extend = function(o2){
-		
+	
 		for(prop in c.prototype){
 			
 			if(!o2[prop]){
 				o2[prop] = c.prototype[prop];
 			} 
 		}
-		 
-		var n = sb.Class(o2);
 		
-		n.prototype.isa = function(t){
-			var types = [this.type];
-			var parent = this.parent;
-			do{
-				types.push(parent.type);
-			} while (parent = parent.parent);
+		var n = new sb.Class(o2);
 		
-			return types.inArray(t);
-			
-		}
-		
-		for(var prop in o2){
-			n.prototype[prop] = o2[prop];
-		}
-		n.prototype.parent = this.prototype;
 	
-		n.prototype.parent.init = function(){
-			
-			return o.init.apply(n.prototype, arguments);
+		n.prototype.getParentInit = function(){
+			return o.init;
 		};
 		
+		n.prototype.isa = function(t){
+			
+			
+			return this.getParents().inArray(t);
+			
+		}
+
+		n.prototype.getParents = function(x){
+			parents = [this.type];
+			var parent = this.parent;
+			do{
+				parents.push(parent.type);
+			} while (parent = parent.parent);
+
+			return parents;
+		};
+		
+		n.prototype.parent = this.prototype;
+		
+		n.prototype[o.type] = {
+			init  : this.prototype.init
+		};
+	
+		/*n.prototype[o.type] = {
+			init  : this.prototype.init
+		};*/
 		return n;
 	};
 	
+	
 	return c;
 };
+
+var Person = sb.Class({
+	init : function(name){
+		this.name = name;
+	},
+	type : 'Person',
+	say : function(){
+		sb.consol.log('I am an person');
+	}
+});
+
+var Employee = Person.extend({
+	init : function(name, id){
+	
+		this.name = name;
+		//this.Person.init.call(this, name, id);
+		this.id = id;
+	},
+	type : 'Employee',
+	say : function(){
+		
+		this.parent.say();
+		sb.consol.log('I am an employee');
+	}
+});
+
+var Manager = Employee.extend({
+	init : function(name, id, super_id){
+		this.Employee.init.call(this, name, id);
+		//this.Employee.init.call(this, name, id);
+		this.super_id = super_id;
+	},
+	type : 'Manager',
+	say : function(){
+		this.parent.say();
+		sb.consol.log('I am a manager');
+	}
+});
