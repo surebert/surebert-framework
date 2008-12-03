f413c9e3182cb648ea734029070bcabf43ad6b99
visco <visco@874840ac-8b26-453e-a097-40cf495e22eb>
2008-12-03 18:23:25 +0000

    added sb.nodeList.prototype.stylesTrail which allows for an eefect to be applied and removed from a series of nodes over a time

---
+++ nodeList/prototype/stylesTrail.js
@@ -0,0 +1,60 @@
+/**
+@Name: sb.nodeList.prototype.stylesTrail
+@Description: Cahnegs the styles of a nodelist in order that they appear in the nodelist on a timeout, and then changes the styles back to how their were
+@Example:
+	$('li').stylesTrail({
+		styles : {
+			backgroundColor : 'orange',
+			border : '2px dotted red'
+		},
+		offset : 6,
+		onEnd : function(){
+			
+		}
+	});
+ */
+sb.nodeList.prototype.stylesTrail = function(params){
+	styles = params.styles || {backgroundColor : 'orange'};
+	offset = params.offset || 300;
+	offsetOff = params.offsetOff || 80;
+	
+	var i = 0; 
+	var count = this.length;
+	var j = 0;
+	var self = this;
+	
+	this.forEach(function(node){
+		node._origStyles = {};
+		for(prop in styles){
+			if(styles.hasOwnProperty(prop)){
+				node._origStyles[prop] = node.getStyle(prop);
+			}
+		}
+		
+		window.setTimeout(function(){
+			for(prop in styles){
+				if(styles.hasOwnProperty(prop)){
+					node.setStyle(prop, styles[prop]);
+				}
+			}
+			window.setTimeout(function(){
+				for(prop in node._origStyles){
+					if(styles.hasOwnProperty(prop)){
+						node.setStyle(prop, node._origStyles[prop]);
+					}
+				}
+				
+				j++;
+				
+				if(j == count && typeof params.onEnd == 'function'){
+					params.onEnd(self);
+				}
+			}, i+offset);
+		}, i);
+		
+		i += offsetOff;
+		
+	});
+
+	return this;
+};
