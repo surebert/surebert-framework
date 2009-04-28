676ee0d9e6867a13fa184e79966c4300b4eba5a4
visco <visco@02073c57-076e-4e5e-aebc-6644ba4999ab>
2009-04-28 21:51:57 +0000

    adding zend framework autoloader

---
+++ Zend/Loader.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Used to load the Zend framework and enable autoload
+ * @version: 1.0 04/28/2009 04/28/2009
+ */
+
+class sb_Zend_Loader{
+
+    public function __construct($path){
+        
+        //checks to make sure this didn't already happen
+        if(!class_exists('Zend_Loader')){
+            //add Zend framework to the include path otherwise, internal requires won't work
+            set_include_path($path . PATH_SEPARATOR . get_include_path());
+
+            //load the zf autoloader
+            require($path.'/Zend/Loader.php');
+            Zend_Loader::registerAutoload();
+
+        }
+    }
+
+}
+?>
