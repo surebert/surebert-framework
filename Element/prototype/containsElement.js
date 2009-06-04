4712aafc65ae3eff446c8d6de96e55277f71abf8
visco <visco@874840ac-8b26-453e-a097-40cf495e22eb>
2009-06-04 02:34:22 +0000

    added Element.prototype.containsElement and Element.prototype.isWithin
    /**
    @Name: Element.prototype.containsElement
    @Author: Paul Visco
    @Version: 0.1 5-21-09 06-03-09
    @Description: Determines if this this element contains another.  Inspired by John Resig blog
    @Param: b The other element to check if for
    @Return: returns boolean If this contains b
    @Example:
    $('#one_element').containsElement('#another_element');
    */
    
    /**
    @Name: Element.prototype.containsNode
    @Author: Paul Visco
    @Version: 0.1 5-21-09 06-03-09
    @Description: Determines if this element is within another
    @Param: b The other element to determine if this element is within
    @Return: returns boolean If element b is within this element
    @Example:
    $('#my_element').isWithin('#another_element');
    */

---
+++ Element/prototype/containsElement.js
@@ -0,0 +1,17 @@
+/**
+@Name: Element.prototype.containsElement
+@Author: Paul Visco
+@Version: 0.1 5-21-09 06-03-09
+@Description: Determines if this this element contains another.  Inspired by John Resig blog
+@Param: b The other element to check if for
+@Return: returns boolean If this contains b
+@Example:
+$('#one_element').containsElement('#another_element');
+*/
+Element.prototype.containsElement = function(b){
+    b = $(b);
+
+    return this.contains ?
+    this != b && this.contains(b) :
+    !!(this.compareDocumentPosition(b) & 16);
+};
