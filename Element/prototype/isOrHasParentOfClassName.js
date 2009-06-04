beb3e72b462c630d54bfb9aacc927843e84096f5
visco <visco@874840ac-8b26-453e-a097-40cf495e22eb>
2009-06-04 00:24:22 +0000

    updated formatting in isOrHasParentOfClassName

--- Element/prototype/isOrHasParentOfClassName.js
+++ Element/prototype/isOrHasParentOfClassName.js
@@ -1,5 +1,7 @@
 /**
 @Name: Element.prototype.isOrHasParentOfClassName
+@Author: Paul Visco
+@Version: 0.1 05-10-09 06-03-09
 @Description: Searches the node itself, followed by parentnodes incrementally till it find a node of the specified className
 @Param: string className The className to look for
 @Return: sb.element The element itself or the parentNode that has the className or false if not found
@@ -12,15 +14,15 @@ mySbElement.isOrHasParentOfClassName('highlighted');
 Element.prototype.isOrHasParentOfClassName = function(className){
 
     if(this.hasClassName(className)){
-	return this;
+        return this;
     }
 
     var ret = false;
     var parent = this;
     while(parent = $(parent.parentNode)){
-	if(parent.hasClassName && parent.hasClassName(className)){
-	    return parent;
-	}
+        if(parent.hasClassName && parent.hasClassName(className)){
+            return parent;
+        }
     }
     return ret;
 
