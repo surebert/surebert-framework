4b67fb64645adeefc5f1619332f853d96484d9cc
visco <visco@02073c57-076e-4e5e-aebc-6644ba4999ab>
2009-08-18 19:58:17 +0000

    added new sb_Files::filename_to_mime() which turns a filename string into a mime type

--- Files.php
+++ Files.php
@@ -102,7 +102,7 @@ class sb_Files{
                     break;
                 
                 case 'doc':
-                    $m = 'application/ms-word';
+                    $m = 'application/msword';
                     break;
 
                 case 'xls':
@@ -131,18 +131,28 @@ class sb_Files{
      * @return string The mime type from finfo
      */
     public static function file_to_mime($file){
-
-        if(class_exists('finfo')){
+        
+        $mime = self::filename_to_mime($file);
+        if($mime){
+            return $mime;
+        } else if(class_exists('finfo') && is_file($file)){
             $finfo = @new finfo(FILEINFO_MIME, "/usr/share/misc/magic");
 
             if($finfo){
                 /* get mime-type for a specific file */
                 return $finfo->file($file);
-            } else {
-                $ext = strtolower(end(explode(".", basename($file))));
-                return self::extension_to_mime($file);
             }
         }
+    }
+    /**
+     * Convert a filename to a mime type
+     * @param string $filename
+     * @return string The mime type of the file
+     */
+    public static function filename_to_mime($filename){
+        $arr = explode(".", basename($filename));
+        $ext = strtolower(end($arr));
+        return self::extension_to_mime($ext);
     }
 	
 	/**
