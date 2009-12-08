1d42c02ca3aa82ec13882553ceec8b5f4035f3e0
visco <visco@02073c57-076e-4e5e-aebc-6644ba4999ab>
2009-12-08 21:04:48 +0000

    now cancels events as well allows setting of METHOD constructor changed to accept only uid if you want a specific uid or for canceling event or nothing otherwise

--- ICalendar/Event.php
+++ ICalendar/Event.php
@@ -1,11 +1,14 @@
 <?php
 /**
  * Used to create an .ics doc for an ICalendar event
+ * RFC 2446 http://tools.ietf.org/html/rfc2446
  *
  * Tested with entouage, outlook, mail.app, owa, android, iphone, and blackbery
- * 
+ *
  * <code>
- * $event = new sb_ICalendar_Event('Discussing the new development plan for the intranet', '901 Room #3');
+ * $event = new sb_ICalendar_Event();
+ * $event->location = '901 Washington #3';
+ * $event->summary = 'Ride for Roswell Meeting';
  * $event->set_time('11/26/2009 13:30', '11/26/2009 14:30');
  * $event->add_attendee(new sb_ICalendar_Attendee('Reid, Delmar', 'del.reid@roswellpark.org'));
  * $event->add_attendee(new sb_ICalendar_Attendee('Dean, Gregary', 'gregary.dean@roswellpark.org'));
@@ -20,18 +23,55 @@
  *
  * //send via email
  * $event->send();
+ *
+ * //Canceling
+ * $event = new sb_ICalendar_Event($uid);
+ * $event->summary = 'Ride for Roswell Meeting';
+ * $event->add_attendee(new sb_ICalendar_Attendee('Reid, Delmar', 'del.reid@roswellpark.org'));
+ * $event->add_attendee(new sb_ICalendar_Attendee('Dean, Gregary', 'gregary.dean@roswellpark.org'));
+ * $event->set_organizer(new sb_ICalendar_Organizer('Visco, Paul', 'paul.visco@roswellpark.org'));
+ *
+ * $event->send();
  * </code>
  *
  * @package sb_ICalendar
- * @author paul.visco@roswellpark.org
+ * @author paul.visco@roswellpark.org, gregary.dean@roswellpark.org
  */
-class sb_ICalendar_Event{
+class sb_ICalendar_Event {
 
 	/**
 	 * The summary of the event
 	 * @var string
 	 */
-	protected $summary ='';
+	public $summary ='';
+
+	/**
+	 * The location of the event
+	 * @var string
+	 */
+	public $location ='';
+
+	/**
+	 * The method that the event is sent as
+	 *
+	 * REQUEST = new event
+	 * CANCEL = cancel event
+	 * see rfc 2446 for more info http://tools.ietf.org/html/rfc2446#section-3.2
+	 *
+	 * @var string
+	 */
+	public $method = 'REQUEST';
+
+	/**
+	 * The unique event id
+	 * You need this id in order to cancel or update the event, it is also what
+	 * makes the event unique on a calendar.
+	 *
+	 * If not provided it is calculated from the md5 of the start and end time plus summary
+	 *
+	 * @var string
+	 */
+	public $uid;
 
 	/**
 	 * The start time of the event in any format strtotime can handle
@@ -59,14 +99,11 @@ class sb_ICalendar_Event{
 
 	/**
 	 * Set up the basic event parameters
-	 * @param string $summary A summary of the event
-	 * @param string $dtstart The begin time of the event in any format strtotime can handle
-	 * @param string $dtend The endtime of the event in any format strtotime can handle
-	 * @param string $location Optional The location of the event
+	 * @param string $uid The unqiue ID of the event, assigned if not provided
+	 * required for cancel, update
 	 */
-	public function __construct($summary, $location=''){
-		$this->summary = $summary;
-		$this->location = $location;
+	public function __construct($uid=''){
+		$this->uid = $uid;
 
 	}
 
@@ -75,7 +112,7 @@ class sb_ICalendar_Event{
 	 * @param string $dtstart The begin time of the event in any format strtotime can handle
 	 * @param string $dtend The endtime of the event in any format strtotime can handle
 	 */
-	public function set_time($dtstart, $dtend){
+	public function set_time($dtstart, $dtend) {
 		$this->dtstart = $dtstart;
 		$this->dtend = $dtend;
 	}
@@ -85,7 +122,7 @@ class sb_ICalendar_Event{
 	 *
 	 * @param sb_ICalendar_Attendee $attendee
 	 */
-	public function set_organizer(sb_ICalendar_Organizer $attendee){
+	public function set_organizer(sb_ICalendar_Organizer $attendee) {
 		$this->organizer = $attendee;
 	}
 
@@ -96,14 +133,14 @@ class sb_ICalendar_Event{
 	 *
 	 * @param sb_ICalendar_Attendee $attendee
 	 */
-	public function add_attendee(sb_ICalendar_Attendee $attendee){
+	public function add_attendee(sb_ICalendar_Attendee $attendee) {
 		$this->attendees[] = $attendee;
 	}
 
 	/**
 	 * Sends HTML headers used to make browser recognize .ics file
 	 */
-	public function send_html_headers(){
+	public function send_html_headers() {
 
 		header('Content-type: text/calendar; charset=utf-8');
 		header('Content-Disposition: inline; filename=calendar.ics');
@@ -113,7 +150,7 @@ class sb_ICalendar_Event{
 	 * Saves the ics packet as a file
 	 * @param string $file_path
 	 */
-	public function to_file($file_path){
+	public function to_file($file_path) {
 		$ics = $this->__toString();
 		file_put_contents($file_path, $ics);
 	}
@@ -122,22 +159,35 @@ class sb_ICalendar_Event{
 	 * Send via email the subject is the first 20 chars of the summary,
 	 * the message is the summary.  The email is sent to the organizer's email,
 	 * The attendees all cc'd
-	 * 
+	 *
 	 * @return boolean
 	 */
-	public function send(){
+	public function send() {
+		$subject = 'EVENT';
+		if($this->method == 'CANCEL'){
+
+			$subject = 'CANCELED '.$subject;
+			if(empty($this->uid)){
+				throw(new Exception('Must set uid to cancel an event.'));
+			}
+		}
+
+		if(!empty($this->summary)){
+			$subject .= ': '.substr($this->summary, 0, 20).'...';
+		}
 
 		$to = '"'.$this->organizer->dname.'" <'.$this->organizer->email.'>';
-		$mail = new sb_Email($to, "EVENT: ".substr($this->summary, 0, 20).'...', $this->summary, $to);
+
+		$mail = new sb_Email($to, $subject, $this->summary, $to);
 		$attendee_emails = Array();
-		foreach($this->attendees as $attendee){
+		foreach($this->attendees as $attendee) {
 			$attendee_emails[] = '"'.$attendee->dname.'" <'.$attendee->email.'>';
 		}
 
 		$mail->cc = $attendee_emails;
 		$mail->add_ICalendar_Event($this);
 		return $mail->send();
-		
+
 	}
 
 	/**
@@ -146,14 +196,14 @@ class sb_ICalendar_Event{
 	 */
 	public function  __toString() {
 
-		if(empty($this->organizer)){
+		if(empty($this->organizer)) {
 			throw(new Exception('You must add an event organizer'));
 		}
 
-		if(empty($this->attendees)){
+		if(empty($this->attendees)) {
 			throw(new Exception('You must add at least one attendee'));
 		}
-		
+
 		//convert to unix time
 		$dtstart = strtotime($this->dtstart);
 		$dtend = strtotime($this->dtend);
@@ -161,28 +211,34 @@ class sb_ICalendar_Event{
 		$ics = Array();
 		$ics[] = "BEGIN:VCALENDAR";
 		$ics[] = "VERSION:2.0";
-		$ics[] = "METHOD:REQUEST";
+		$ics[] = "METHOD:".$this->method;
+
+		if($this->method == 'CANCEL') {
+			$ics[] = 'STATUS:CANCELLED';
+		}
+
 		$ics[] = "PRODID:-//surebert/ics//NONSGML v1.0//EN";
 		$ics[] = "BEGIN:VEVENT";
-		
-		if(!empty($this->location)){
+
+		if(!empty($this->location)) {
 			$ics[] = "LOCATION:".$this->location;
 		}
 
-		if(isset($this->organizer)){
+		if(isset($this->organizer)) {
 			$ics[] = $this->organizer->__toString();
 		}
 
-		foreach($this->attendees as $attendee){
+		foreach($this->attendees as $attendee) {
 			$ics[] = $attendee->__toString();
 		}
-		
-		$ics[] = "UID:" . md5($dtstart.$dtend.$this->summary);
+
+		$this->uid = empty($this->uid) ? md5($dtstart.$dtend.$this->summary) : $this->uid;
+		$ics[] = "UID:" . $this->uid;
 		$ics[] = "DTSTAMP:" . gmdate('Ymd').'T'. gmdate('His') . "Z";
 		$ics[] = "DTSTART:" . gmdate('Ymd', $dtstart).'T'. gmdate('His', $dtstart) . "Z";
 		$ics[] = "DTEND:" . gmdate('Ymd', $dtend).'T'. gmdate('His', $dtend) . "Z";
 		$ics[] = "SUMMARY:".$this->summary;
-		
+
 		$ics[] = "END:VEVENT";
 		$ics[] = "END:VCALENDAR";
 
