bc3c0fc6326e00621ccaae6d1df66ad9180da93c
visco <visco@874840ac-8b26-453e-a097-40cf495e22eb>
2010-01-21 16:29:27 +0000

    You can now switch between types of terminal

--- widget/terminal.js
+++ widget/terminal.js
@@ -1,66 +1,92 @@
+sb.include('String.prototype.trim');
+
 sb.widget.terminal = function(params){
 	sb.objects.infuse(params, this);
 	var self = this;
 	var type = this.type || 'input';
 	self.className = self.className || 'sb_terminal';
-	this.textField = new sb.element({
-		tag : type,
-		type: 'text',
-		value : '',
-		className : self.className,
-		styles : {
-			width : '100%',
-			backgroundColor : 'black',
-			color: 'lime',
-			fontFamily : 'courier',
-			border : 0,
-			fontSize : '14px',
-			padding : '5px'
-		},
-		events : {
-			
-			keydown : function(e){
-
-				var target = e.target;
-				var data = [];
-
-				switch(e.keyCode){
-					case 13:
-						if(self.type == 'input' || self.type == 'textarea' && e.shiftKey){
-							self.process(e);
-							e.preventDefault();
-						}
-						
-						break;
-
-					case 27:
-						target.value = '';
-						target.select();
-
-					case 38:
-						var command = self.stack.pop();
-						if(command){
-							target.value = command;
-						}
-
-				}
-			}
-
-		}
-
-	});
-
-	if(type == 'textarea'){
-		sb.include('forms.textarea');
-		sb.include('forms.textarea.allowTabs');
-		this.textField.evt('keydown', sb.forms.textarea.allowTabs);
-	}
-
+	
+	this.create(type);
 	this.stack = [];
-	this.textField.appendToTop('body');
+	
 };
 
 sb.widget.terminal.prototype = {
+	create : function(type){
+		var self = this;
+		type = type || 'input';
+		if(this.textField){
+			this.textField.remove();
+			this.textField = null;
+		}
+		this.textField = new sb.element({
+			tag : type,
+			type: type,
+			value : '',
+			className : self.className,
+			styles : {
+				width : '100%',
+				backgroundColor : 'black',
+				color: 'lime',
+				fontFamily : 'courier',
+				border : 0,
+				fontSize : '14px',
+				padding : '5px'
+			},
+			events : {
+
+				keydown : function(e){
+
+					var target = e.target;
+					var data = [];
+					var value = this.value.trim();
+
+					if(value == 'textarea' || value == 'input'){
+						self.create(value);
+						e.preventDefault();
+						return true;
+					}
+					
+					switch(e.keyCode){
+						case 13:
+							if(self.type == 'input' || self.type == 'textarea' && e.shiftKey){
+								self.process(e);
+								e.preventDefault();
+							}
+
+							break;
+
+						case 27:
+							target.value = '';
+							target.select();
+
+						case 38:
+							var command = self.stack.pop();
+							if(command){
+								target.value = command;
+							}
+
+					}
+				}
+
+			}
+
+		});
+
+		this.textField.appendToTop('body');
+
+		if(type == 'textarea'){
+			sb.include('forms.textarea');
+			sb.include('forms.textarea.allowTabs');
+			this.textField.evt('keydown', sb.forms.textarea.allowTabs);
+		}
+
+		if(typeof self.onAfterCreate == 'function'){
+			self.onAfterCreate();
+		}
+		
+		this.textField.focus();
+	},
 	process : function(e){
 		var self = this;
 		var data = '';
