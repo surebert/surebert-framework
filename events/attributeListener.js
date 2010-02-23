9f4a9d5082dbc8b2e5745fc88251de36a2c918e0
visco <visco@874840ac-8b26-453e-a097-40cf495e22eb>
2010-02-23 18:44:39 +0000

    listeners are sharing sb.events.listener as base

--- events/attributeListener.js
+++ events/attributeListener.js
@@ -1,38 +1,39 @@
 sb.include('events.observer');
 sb.include('events.listener');
 /**
-@Name: sb.events.classListener
+@Name: sb.events.attributeListener
 @Description: Used to create an eventlistener that responds to events on nodes
 with a specific className or combo of classNames in order.  Observes by default
 @Param: object params Used to preseed the listener with class listeners
 @Return: type desc
 @Example:
-var myListener = new sb.events.classListener({
+var myListener = new sb.events.attributeListener({
+	attribute: 'some_attribute',
 	mousemove : {
-		some_classname : function(e){
+		some_attribute : function(e){
 			console.log('move '+ new Date());
 		}
 	},
 	click : {
-		some_classname : function(e){
+		some_attribute : function(e){
 			alert('click');
 		}
 	}
 });
 
 */
-sb.events.classListener = sb.events.listener;
-sb.events.classListener.prototype.delegate = function(e){
+sb.events.attributeListener = sb.events.listener;
+sb.events.attributeListener.prototype.delegate = function(e){
 	var target = e.target;
-	var cl = target.className;
-	if(cl == ''){return false;}
+	var attr = target.getAttribute(this.attribute);
+	if(!attr){return false;}
 
-	var cl = cl.replace(' ', '_');
+	attr = attr.replace(' ', '_');
 
 	var type = e.type;
 
-	if(this[type] && typeof this[type][cl] == 'function'){
+	if(this[type] && typeof this[type][attr] == 'function'){
 
-		this[type][cl](e);
+		this[type][attr](e);
 	}
 };
--- events/idListener.js
+++ events/idListener.js
@@ -1,4 +1,5 @@
 sb.include('events.observer');
+sb.include('events.listener');
 /**
 @Name: sb.events.idListener
 @Description: Used to create an idListener that responds to events on nodes.
@@ -19,64 +20,17 @@ var myListener = new sb.events.idListener({
 });
 
 */
-sb.events.idListener = function(params){
+sb.events.idListener = sb.events.listener;
+sb.events.idListener.prototype.delegate = function(e){
+	var target = e.target;
+	if(target.id == ''){return false;}
 
-	sb.objects.infuse(params, this);
-	var self = this;
-	this.observe();
-};
+	var id = target.id.replace(' ', '_');
 
-sb.events.idListener.prototype = {
-	/**
-	@Name: sb.events.classListener.observe
-	@Description: Observes the events the object has. You must rerun this if you are
-	adding new events.  Not required if just adding new classname to manage.
-	@Return: object The event observer
-	@Example:
-	myListener.unobserve();
-	*/
-	observe : function(){
-		var self = this;
-		this.events = {};
-		['click', 'mouseup', 'mousedown', 'dblclick', 'submit', 'keydown', 'keyup', 'keypress', 'mousemove', 'mouseover', 'mouseout'].forEach(function(evt){
+	var type = e.type;
 
-			if(self[evt]){
-				self.events[evt] = function(e){
-					self.delegate(e);
-				}
-			}
-		});
+	if(this[type] && typeof this[type][id] == 'function'){
 
-
-		return sb.events.observer.observe(this);
-	},
-
-	/**
-	@Name: sb.events.classListener.observe
-	@Description: Observes the events the object has
-	@Return: object The event observer
-	@Example:
-	myListener.unobserve();
-	*/
-	unobserve : function(){
-		return sb.events.observer.unobserve(this);
-	},
-
-	/**
-	@Name: sb.events.classListener.delegate
-	@Description: Used Internally to determine if target has className
-	*/
-	delegate : function(e){
-		var target = e.target;
-		if(target.id == ''){return false;}
-
-		var id = target.id.replace(' ', '_');
-
-		var type = e.type;
-
-		if(this[type] && typeof this[type][id] == 'function'){
-
-			this[type][id](e);
-		}
+		this[type][id](e);
 	}
 };
