652720fd7745990442ffcadd3e8ecdb86b8f0ecb
visco <visco@874840ac-8b26-453e-a097-40cf495e22eb>
2010-04-06 15:40:45 +0000

    added duration argument to pulseBg

--- Element/prototype/pulseBg.js
+++ Element/prototype/pulseBg.js
@@ -1,25 +1,26 @@
-Element.prototype.pulseBg = function(beginColor, endColor, timesToPulse){
-		var t = this;
-		var f = 0;
-		function pulse(){
+Element.prototype.pulseBg = function(beginColor, endColor, timesToPulse, duration){
+	duration = duration || 80;
+	var t = this;
+	var f = 0;
+	function pulse(){
 
-			t.cssTransition([
-				{
-					prop : 'backgroundColor',
-					begin : beginColor,
-					end : endColor,
-					onEnd : function(){
-						if(f < timesToPulse){
-							pulse();
-							f++;
-						} else {
-							pulse = null;
-						}
+		t.cssTransition([
+			{
+				prop : 'backgroundColor',
+				begin : beginColor,
+				end : endColor,
+				onEnd : function(){
+					if(f < timesToPulse){
+						pulse();
+						f++;
+					} else {
+						pulse = null;
 					}
-
 				}
-			], 150).start();
 
-		}
-		pulse();
+			}
+		], duration).start();
+
+	}
+	pulse();
 };
