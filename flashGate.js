05b7df22e28eaad40ac51ac38ffd918d14406edf
visco <visco@874840ac-8b26-453e-a097-40cf495e22eb>
2010-05-06 15:08:48 +0000

    got rid of new sb.flashGate

--- flashGate.js
+++ flashGate.js
@@ -2,13 +2,11 @@
 @Name: sb.flashGate
 @Description: Used to include swf for surebert communicate with flash player for sound, multi-file/progress upload and storage.  If you want to use one of these functions on page load, (e.g. play music) make sure to wrap it in the function sb_onFlashGateLoaded so that it fires once sb.flashGate has loaded.
 @Author: Paul Visco
-@Version: 4.2 02/12/06 09/03/08
-#############################
 */
 sb.include('swf');
 
 sb.flashGate = new sb.swf({
-	src : sb_FlashGateSrc || sb.base+"/FlashGate.swf",
+	src : sb.base+"/FlashGate.swf",
 	//src : '${swf}.swf?d='+Math.random(),
 	width : 1,
 	height : 1,
