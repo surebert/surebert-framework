1a694bba9403a0a95254b3390330bf2766f4f390
visco <visco@874840ac-8b26-453e-a097-40cf495e22eb>
2010-08-20 00:42:37 +0000

    changed refresh to redraw

--- Element/prototype/redraw.js
+++ Element/prototype/redraw.js
@@ -1,12 +1,12 @@
 /**
-@Name: Element.prototype.refresh
+@Name: Element.prototype.redraw
 @Description: Used to refresh an element display by hiding it and reshowing to fix reflow problem in IE
 @Example:
 
 //return the elemenet
-myElement.refresh();
+myElement.redraw();
 */
-Element.prototype.refresh = function(){
+Element.prototype.redraw = function(){
 	var d = this.style.display;
 	this.style.display = 'none';
 	this.style.display = d;
