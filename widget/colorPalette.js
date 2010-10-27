3acdc798ce5aada231cda8ed9058b1f4a74eabc0
visco <visco@874840ac-8b26-453e-a097-40cf495e22eb>
2010-10-27 00:43:50 +0000

    updates to accept more arguments

--- widget/colorPalette.js
+++ widget/colorPalette.js
@@ -7,49 +7,55 @@
 
 sb.include('widget.colorPalette');
 
-var table = new sb.widget.colorPalette('jimmy');
-table.evt('click', function(e){
-	
-	var td = e.target;
-	if(td.nodeName =='TD'){
-			if(table.selectedColor){
-				table.selectedColor.style.border='';
+var table = new sb.widget.colorPalette({
+	id : 'jimmy',
+	events : {
+		click : function(e){
+			if(td.nodeName =='TD'){
+					if(table.selectedColor){
+						table.selectedColor.style.border='';
+					}
+				$('body').style.backgroundColor=td.style.backgroundColor;
+				td.style.border='1px solid white';
+				table.selectedColor = td;
 			}
-		$('body').style.backgroundColor=td.style.backgroundColor;
-		td.style.border='1px solid white';
-		table.selectedColor = td;
+		}
 	}
-	
 });
+
 table.appendToTop('body');
 */
-sb.widget.colorPalette = function(id){
-	var color,td;
-	var colors = ['00','33','66','99','CC','FF'];
-	//var colors = ['00','11','22','33','44','55', '66', '77', '88', 'AA', 'BB', 'CC', 'DD', 'EE', 'FF'];
+sb.widget.colorPalette = function(o){
+	this.colors = o.colors || ['00','22','44', '66', '88', 'BB',  'DD', 'FF'];
+	
 	var table = new sb.element({
 		tag : 'table',
-		id : id || 'sb_web_safe_palette'
+		id : o.id || 'sb_web_safe_palette',
+		events : o.events || {}
 	});
 	
-	var tbody = document.createElement('tbody');
+	var td,tbody = document.createElement('tbody');
 	table.appendChild(tbody);
 		
-	for (var i = colors.length-1; i >= 0; i--) {
+	for (var i = this.colors.length-1; i >= 0; i--) {
 		var tr = document.createElement('tr');
 		tbody.appendChild(tr);
 		
-		for (var j = colors.length-1; j >= 0; j--) {
+		for (var j = this.colors.length-1; j >= 0; j--) {
 			
-			for (var k= colors.length-1; k >= 0; k--) {
-				color = colors[i]+colors[j]+colors[k];
+			for (var k= this.colors.length-1; k >= 0; k--) {
+				color = this.colors[i]+this.colors[j]+this.colors[k];
 				td = document.createElement('td');
 				tr.appendChild(td);
 				td.style.backgroundColor = '#'+color;
+				td.style.width = '2px';
+				td.style.height = '2px';
+				td.style.fontSize = '0px';
+				td.style.cursor = 'pointer';
 			}
 		}
 		
-		color = colors[i]+colors[i]+colors[i];
+		color = this.colors[i]+this.colors[i]+this.colors[i];
 		td = document.createElement('td');
 		tr.appendChild(td);
 		
