ea4e1b67a58761ad0f6c77450db4ad25e7114be5
visco <visco@874840ac-8b26-453e-a097-40cf495e22eb>
2010-11-30 21:23:58 +0000

    added individual errorMessage string, onValid and onInValid methods to the validators so that the validtions and responses could be housed together.  The default is now the red/green rollup thing

--- forms/inputValidator.js
+++ forms/inputValidator.js
@@ -1,3 +1,16 @@
+sb.validation = function(o){
+	sb.objects.infuse(o, this);
+};
+sb.validation.prototype = {
+	//func(input) or regex
+	validator : null,
+	//func(input)
+	onValid : null,
+	//string
+	errorMessage : null
+};
+
+
 /**
 @Name: sb.forms.inputValidator
 @Description: Validates inputs based on validate attribute
@@ -7,28 +20,61 @@ validations object of regex or function properties, these are executed or matche
 onValid(input) function Fires when the input is validate if it is valid
 onInValid(input) function Fires when the input is not validate
 @Example:
- var validator = new sb.forms.inputValidator({
+var ptsui = {};
+
+ptsui.validator = new sb.forms.inputValidator({
 	validations : {
-		//simple regex validation
-		acct : /^\d\.\d{2}$/,
-		phone : /\d{3}-\d{3}-\d{4}/,
-		number : /^\d+$/,
-		email : /\b(^(\S+@).+(\.\w+)$)\b/gi
+		acct : new sb.validation({
+			validtor : /^\d\.\d{2}$/,
+			errorMessage :  'Sorry this does not match an acct number e.g. 4.32'
+		}),
+		phone : new sb.validation({
+			validtor :/^\d{3}-\d{3}-\d{4}$/,
+			errorMessage :  'Sorry this does not match a phone number e.g. 716-877-9999'
+		}),
+		email : new sb.validation({
+			validtor : /\b(^(\S+@).+(\.\w+)$)\b/ig,
+			errorMessage :  'Sorry this does not match a phone number e.g. test@test.com'
+		}),
+		at_least_one : new sb.validation({
+			validtor : function(input){
+				var inputs = $("input[name='"+input.name+"']");
+				return inputs.some(function(v){return v.checked;});
+			},
+			onValid : function(input){
+				var inputs = $("input[name='"+input.name+"']");
+				inputs.forEach(function(v){
+					if(v.errorMessageP){
+						v.errorMessageP.rollUp();
+					}
+				});
+			},
+
+			onInValid : function(input){
+				var inputs = $("input[name='"+input.name+"']");
+				sb.forms.inputValidator.prototype.onInValid(inputs.nodes[0]);
+			},
+			errorMessage :  'You must select at least one'
+		})
 	},
+
 	validateOnKeyUp : true,
-	onValid: function(input){
-		input.style.backgroundColor = 'lime';
-	},
-	onInValid: function(input){
-		input.style.backgroundColor = 'red';
+
+	onKeyDown : function(e) {
+		//not allow pipe
+		if(e.keyCode == 220){
+			e.preventDefault();
+		}
 	}
+
 });
+
+<input type="text" validate="acct" required="1" name="acct" />
 */
 
 sb.forms.inputValidator = function(o){
-	sb.objects.infuse(o, this);
 	var self = this;
-
+	sb.objects.infuse(o, this);
 	this.validateOnKeyUp = this.validateOnKeyUp === false ? this.validateOnKeyUp : true;
 	this.onKeyUpEvt = sb.events.add(document, 'keyup', function(e){
 	
@@ -48,11 +94,14 @@ sb.forms.inputValidator = function(o){
 		}
 	});
 
+	this.parent = sb.forms.inputValidator.prototype;
 };
 
 sb.forms.inputValidator.prototype = {
+	 validateOnKeyUp: true,
 	_validate : function(e){
-		var input = e.target;
+		var input = e.nodeName ? e : e.target;
+
 		if(input.nodeName == 'INPUT'){
 			input.value = input.value.replace(/(^\s+|\s+$)/g, '');
 		}
@@ -64,27 +113,42 @@ sb.forms.inputValidator.prototype = {
 		}
 
 		var validate = input.getAttribute('validate');
-		input.validationErrors = [];
+		var required = input.getAttribute('required') || 0;
+
 		if(validate){
+			var validation  = this.validations[validate];
+			if(!validation){return false;}
 			var self = this;
-			var validationTypes = validate.split(' ');
-			validationTypes.forEach(function(validationType){
-
-				var validation = self.validations[validationType];
-
-				if(typeof validation == 'function'){
-					input.valid = validation(input);
-				} else {
-					input.valid = input.value.match(validation);
+			input.valid = false;
+			//if optional
+			if(input.value === '' && required === '0'){
+				input.valid = true;
+			} else if(input.value !== ''){
+				if(this.validations[validate]){
+					
+					if(typeof validation.validtor == 'function'){
+						input.valid = validation.validtor(input);
+					} else {
+						input.valid = input.value.match(validation.validtor);
+					}
 				}
-
-				if(input.valid){
+			}
+			
+			if(input.valid){
+				if(typeof validation.onValid == 'function'){
+					validation.onValid(input);
+				} else if(self.onValid){
 					self.onValid(input);
-				} else {
-					input.validationErrors.push(validationType);
+				}
+			} else {
+				input.errorMessage = validation.errorMessage;
+
+				if(typeof validation.onInValid == 'function'){
+					validation.onInValid(input);
+				} else if(self.onInValid){
 					self.onInValid(input);
 				}
-			});
+			}
 			
 		}
 	},
@@ -97,7 +161,13 @@ sb.forms.inputValidator.prototype = {
 		input.style.backgroundColor = 'lime';
 	};
 	*/
-	onValid: function(input){},
+	onValid: function(input){
+		input.style.backgroundColor = 'lime';
+		if(input.errorMessageP){
+			input.errorMessageP.rollUp();
+			input.errorMessageP = false;
+		}
+	},
 
 	/**
 	@Name: sb.forms.inputValidator.prototype.onInValid
@@ -108,7 +178,22 @@ sb.forms.inputValidator.prototype = {
 		input.style.backgroundColor = 'red';
 	};
 	*/
-	onInValid: function(input){},
+	onInValid: function(input){
+		input.style.backgroundColor = 'red';
+		
+		var parent = $(input.parentNode);
+		if(!input.errorMessageP){
+				input.errorMessageP = new sb.element({
+					tag : 'p',
+					innerHTML : '',
+					styles : {
+						color : 'red'
+					}
+				});
+		}
+		input.errorMessageP.innerHTML = input.errorMessage;
+		input.errorMessageP.appendToTop(parent);
+	},
 
 	/**
 	@Name: sb.forms.inputValidator.prototype.onKeyDown
