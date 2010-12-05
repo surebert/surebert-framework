f8d69281f47ff7cb0097f8e36a9d925d94d99094
visco <visco@874840ac-8b26-453e-a097-40cf495e22eb>
2010-12-05 19:00:06 +0000

    particles now adjust element size not just font-size

--- widget/particles.js
+++ widget/particles.js
@@ -1,4 +1,4 @@
-/**
+	/**
 @Name: sb.widget.particles
 @Description: makes floating particles that float around the screen
 Math for floating pattern from code by Altan d.o.o. (snow@altan.hr, http://www.altan.hr/snow/index.html)
@@ -6,7 +6,7 @@ Math for floating pattern from code by Altan d.o.o. (snow@altan.hr, http://www.a
 XHTML compliant will work in when using DOCTYPE XHTML
 
 @Author: Paul Visco 11-05-04, 12-15-08
-@Example: 
+@Example:
 sb.include('widget.particles');
 sb.widget.particles.init(5, 'hello');
 
@@ -30,7 +30,7 @@ sb.widget.particles = {
 			var snd = new sb.sound({url : sb.widget.particles.sound}).play();
 		}
 	},
-	
+
 	container : new sb.element({
 		tag : 'sb_particles',
 		styles : {
@@ -42,64 +42,66 @@ sb.widget.particles = {
 				if(target.nodeName =='SB_PARTICLE' || (target.parentNode && target.parentNode.nodeName =='SB_PARTICLE')){
 					sb.widget.particles.playSound();
 					target.remove();
-							
+
 					sb.widget.particles.removed++;
-					
+
 					if(sb.widget.particles.removed >= sb.widget.particles.total){
 						sb.widget.particles.stopAnimation =1;
 					}
-					
-					
+
+
 				}
 			}
 		}
-		
+
 	}),
-	
+
 	hideParticles : function(){
 		this.floats.hide();
 	},
-	
+
 	animate : function(){
 		if(this.stopAnimation ==1){return;}
 		var len = this.particles.length;
-		for (var i = 0; i < len; ++ i) { 
-				
+		for (var i = 0; i < len; ++ i) {
+
 			this.yp[i] += this.sty[i];
 			if (this.yp[i] > sb.browser.h-50) {
-				
+
 				var rand = Math.random();
 				this.xp[i] = rand*(sb.browser.w-this.am[i]-30);
 				this.yp[i] = 0;
 				this.stx[i] = 0.02 + rand/10;
 				this.sty[i] = 0.7 + rand;
-				
+
 				sb.widget.particles.playSound();
 			}
-		
+
 			this.dx[i] += this.stx[i];
 			var x= this.xp[i] + this.am[i]*Math.sin(this.dx[i]);
 			var y = this.yp[i];
 			this.particles[i].style.fontSize=this.sty[i]*20+'px';
-			
+			this.particles[i].style.width=this.sty[i]*60+'px';
+			this.particles[i].style.height=this.sty[i]*60+'px';
+
 			this.particles[i].style.left = x+'px';
 			this.particles[i].style.top = y+'px';
 		}
 		var self = this;
 		this.animating = window.setTimeout(function(){self.animate();}, 30);
-			
+
 	},
-	
+
 	create : function(){
 		var tl = this.total;
-		for (var i = 0; i < tl; ++ i) {  
+		for (var i = 0; i < tl; ++ i) {
 			this.dx[i] = 0;                       		 // set coordinate variables
 			this.xp[i] = Math.random()*(sb.browser.w-50);  // set position variables
 			this.yp[i] = Math.random()*sb.browser.h;
 			this.am[i] = Math.random()*50;         // set amplitude variables
 			this.stx[i] = 0.03 + Math.random()/10; // set step variables
 			this.sty[i] = 0.7 + Math.random();     // set step variables
-			
+
 			var particle = new sb.element({
 				tag : 'sb_particle',
 				innerHTML : this.content,
@@ -113,15 +115,15 @@ sb.widget.particles = {
 					zIndex : i+1
 				}
 			});
-			
+
 			this.particles.push(particle);
 			particle.appendTo(this.container);
-			
+
 		}
-		
-		
+
+
 	},
-	
+
 	init : function(total, content, sound){
 		this.sound = sound ||'';
 		this.total = total || this.total;
@@ -129,6 +131,6 @@ sb.widget.particles = {
 		this.container.appendTo('body');
 		this.create();
 		this.animate();
-		
+
 	}
 };
