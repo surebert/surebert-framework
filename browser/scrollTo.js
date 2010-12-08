ae0789b24b96fd0c059a6560a6475f328f3da7d6
visco <visco@874840ac-8b26-453e-a097-40cf495e22eb>
2010-12-08 02:09:08 +0000

    updated sb.browser.scrollTo allows for x/y combo scrolling scrolling and new onStopped function

--- browser/scrollTo.js
+++ browser/scrollTo.js
@@ -3,49 +3,84 @@ sb.include('browser.getScrollPosition');
 /**
 @Name:  sb.browser.scrollTo
 @Author: Paul Visco
-@Version: 1.0
 @Description: Used to gradually scroll the browser somewhere
-@Param: integer y Where to scroll to on the y axis
-@Param: integer duration The time milliseconds to scroll over
-Example: 
-//scrolls to y 500 and alerts 'done' at the end
-var scrollTo = sb.browser.scrollTo({y : 500, duration : 24, onEnd : function(){
-	alert('done');
-}});
+@Param: Object with the following properties
+integer y Where to scroll to on the y axis
+integer x Where to scroll to on the x axis
+integer duration The time milliseconds to scroll over
+function onEnd Fires if it successfully completes scrolling
+function onStopped Fires if the user stops the scrolling by initiating their own scrolling
+@Example:
+//scrolls to y 500, x: 200 and alerts 'done' when complete or stopped of the user scrolls
+var scrollTo = sb.browser.scrollTo({
+	y : 500,
+	x : 200,
+	duration : 24,
+	onEnd : function(){
+		alert('done');
+	},
+	onStopped : function(){
+		alert('stopped');
+	});
 
 */
 sb.browser.scrollTo = function(o){
-	if(sb.browser.scrolling){
-		sb.browser.scrolling.reset();
-	}
-	//percent, duration, onEnd
-	sb.browser.scrolling = new sb.effect({
-		onChange : function(){
-			window.scrollTo(0, this.value);
-		}
-	});
-	
-	sb.events.add(document, 'DOMMouseScroll', function(){sb.browser.scrolling.stop();});
-	sb.events.add(document, 'mousewheel', function(){sb.browser.scrolling.stop();});
-	
-	var scrollTo = function(o){
-			var scrollPos = sb.browser.getScrollPosition();
-			sb.browser.scrolling.duration = o.duration || 45;
-			sb.browser.scrolling.begin = scrollPos[1]; 
-			
-			sb.browser.scrolling.onEnd = o.onEnd || 0;
-			
-			if(typeof o.y =='number'){
-				sb.browser.scrolling.change = o.y-sb.browser.scrolling.begin;
+	var scrollPos = sb.browser.getScrollPosition();
+	var x = scrollPos[0];
+	var y = scrollPos[1];
+	o.x = typeof o.x == 'undefined' ? x : o.x;
+	o.y = typeof o.y == 'undefined' ? y : o.y;
+	var stopScroll = function(complete){
+		stop.remove();
+		stop2.remove();
+		window.clearTimeout(scrolling);
+		if(complete){
+			if(typeof o.onEnd == 'function'){
+				o.onEnd();
 			}
-			
-			sb.browser.scrolling.tween = (sb.browser.scrolling.change < 0) ? 'outQuart' : 'inQuart';
-			
-			sb.browser.scrolling.restart();
-			return scrollTo;
+		} else {
+			if(typeof o.onStopped == 'function'){
+				o.onStopped();
+			}
+		}
+
 	};
-	
-	scrollTo(o);
-	return scrollTo;
-	
+	var time = 0;
+	var duration = o.duration || 45;
+	var beginX = x
+	var changeX = o.x-x;
+	var tweenX = changeX < 1 ? 'outQuart': 'inQuart';
+	var beginY = y;
+	var changeY = o.y-y;
+	var tweenY = changeY < 1 ? 'outQuart': 'inQuart';
+	var scrolling = window.setInterval(function(){
+		if(time >= duration){
+			stopScroll();
+			return;
+		}
+		time++;
+
+		if(y != o.y){
+			y = sb.effects.tween[tweenY](time,beginY,changeY,duration);
+		}
+
+		if(x != o.x){
+			x = sb.effects.tween[tweenX](time,beginX,changeX,duration);
+		}
+
+		x = Math.round(x);
+		y = Math.round(y);
+
+		window.scrollTo(x, y);
+		if(x == o.x && y == o.y){
+			stopScroll(true);
+		}
+
+	}, 10);
+
+	var stop = sb.events.add(document, 'DOMMouseScroll', function(){stopScroll(false);});
+	var stop2 = sb.events.add(document, 'mousewheel', function(){stopScroll(false);});
+
+	scrolling.stop = stopScroll;
+	return scrolling;
 };
