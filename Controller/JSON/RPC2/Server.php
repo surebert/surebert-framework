ee5a973119325d458a77e91595d2cefb58062dce
visco <visco@02073c57-076e-4e5e-aebc-6644ba4999ab>
2011-05-06 02:08:45 +0000

    takes advantage of new Gateway::$cmd_options if they exist so that you can debug by setting Gateway::$cmd_options['json_request'] in --config=file file

--- Controller/JSON/RPC2/Server.php
+++ Controller/JSON/RPC2/Server.php
@@ -1,8 +1,7 @@
 <?php
-
 /**
  * Used to response to JSON_RPC2 requests as per the spec proposal at http://groups.google.com/group/json-rpc/web/json-rpc-1-2-proposal
- * @version 1.21 02/06/09 05/12/09
+ * @version 02/06/09 05/05/11
  * @author visco
  * @package sb_JSON_RPC2
  *
@@ -13,7 +12,8 @@ class sb_Controller_JSON_RPC2_Server extends sb_Controller {
 	 * The transport method to listen for data on
 	 * @var string post or get or both
 	 */
-	public $method = 'post';
+	public $method = 'both';
+	
 	/**
 	 * Determines if objects passed as params are converted to hashes instead of objects
 	 * @var boolean
@@ -69,11 +69,12 @@ class sb_Controller_JSON_RPC2_Server extends sb_Controller {
 	 * </code>
 	 */
 	public function __construct($methods=Array()) {
+		
 		if (isset($_SERVER) && isset($_SERVER['HTTP_PHP_SERIALIZE_RESPONSE'])) {
 			$this->php_serialize_response = true;
 		}
 	}
-
+	
 	/**
 	 * Determines is gzencoding is used
 	 * @param $level Integer A number between 0-9, the compression level, higher takes longer
@@ -102,181 +103,6 @@ class sb_Controller_JSON_RPC2_Server extends sb_Controller {
 	 */
 	public function set_logger(sb_Logger_Base $logger) {
 		$this->logger = $logger;
-	}
-
-	/**
-	 * Parses the request
-	 * @param $json_request
-	 */
-	protected function parse_request($debug_request='') {
-
-		$response = new sb_JSON_RPC2_Response();
-
-		$request = null;
-
-		$input = '';
-
-		//DEBUGGING
-		if (!empty($debug_request)) {
-			$request = new sb_JSON_RPC2_Request($debug_request);
-			$input = $debug_request;
-
-			//FROM POST DATA
-		} else if ($this->method == 'post' || $this->method == 'both') {
-			$post = file_get_contents("php://input");
-
-			if (isset($this->encryption_key)) {
-				$post = $this->encryptor->decrypt($post);
-			}
-
-			$request = new sb_JSON_RPC2_Request($post);
-			$input = $post;
-		}
-
-		$get = Gateway::$request->get;
-
-		//FROM GET DATA
-		if (is_null($request)
-				&& ($this->method == 'get' || $this->method == 'both')
-				&& isset($get['method']) && isset($get['params']) && isset($get['id'])
-		) {
-
-			$request = new sb_JSON_RPC2_Request();
-			$request->method = $get['method'];
-
-			$params = $get['params'];
-
-			if (!preg_match("~[\[\{]~", substr($params, 0, 1))) {
-
-				$params = base64_decode($params);
-			}
-
-			$request->params = json_decode($params);
-
-			$request->id = $get['id'];
-			$input = json_encode($request);
-		}
-
-		if (is_null($request)) {
-			$response->error = new sb_JSON_RPC2_Error(-32700, 'Parse Error', "Data Received: " . $input);
-		} else {
-			$response->id = $request->id;
-		}
-
-		if ($this->logger instanceof sb_Logger) {
-			$this->logger->add_log_types(Array('sb_json_rpc2_server'));
-			$this->logger->sb_json_rpc2_server("--> " . $input);
-		}
-		$servable = false;
-		if (method_exists($this, $request->method)) {
-			$reflection = new ReflectionMethod($this, $request->method);
-
-			//check for phpdocs
-			$docs = $reflection->getDocComment();
-			$servable = false;
-			if (!empty($docs)) {
-				if (preg_match("~@servable (true|false)~", $docs, $match)) {
-					$servable = $match[1] == 'true' ? true : false;
-				}
-			}
-		}
-		//check for requested remote procedure
-		if ($servable) {
-
-			if (is_object($request->params)) {
-				$answer = call_user_func(Array($this, $request->method), $request->params);
-			} else if (is_array($request->params)) {
-				$answer = call_user_func_array(Array($this, $request->method), $request->params);
-			}
-
-			//if they return an error from the method call, return that
-			if ($answer instanceof sb_JSON_RPC2_Error) {
-				$response->error = $answer;
-			} else {
-				//otherwise return the answer
-				$response->result = $answer;
-			}
-		} else {
-
-			$response->error = new sb_JSON_RPC2_Error();
-			$response->error->code = -32601;
-			$response->error->message = "Procedure not found";
-		}
-
-		//remove unnecessary properties
-		if ($response->error instanceof sb_JSON_RPC2_Error) {
-
-			unset($response->result);
-			if (is_null($response->error->data)) {
-				unset($response->error->data);
-			}
-		} else {
-			unset($response->error);
-		}
-
-		//log the final response
-		if ($this->logger instanceof sb_Logger) {
-			$this->logger->sb_json_rpc2_server('<-- ' . json_encode($response));
-		}
-
-
-		return $response;
-	}
-
-	/**
-	 * Serves data based on the json_request if set, otherwise based on Gateway::$request->post['sb_JSON_RPC2_Request']
-	 * @param $json_request String This optional argument can be used for debugging the server.  A sb_JSON_RPC2_Request formatted JSON string e.g. {"method":"add","params":[1,2],"id":"abc123"}
-	 * @return string JSON encoded sb_JSON_RPC2_Response
-	 */
-	public function handle($json_request = null) {
-
-		$response = $this->parse_request($json_request);
-
-		$message = 'OK';
-		$status = 200;
-		//headers from spec here http://json-rpc.googlegroups.com/web/json-rpc-over-http.html
-		if (isset($response->error) && $response->error instanceof sb_JSON_RPC2_Error) {
-			$code = $response->error->code;
-
-			if (in_array($code, Array(-32700, -3260, -32603))
-				|| ($code <= -32000 && $code >= -32099)) {
-				$status = 500;
-				$message = 'Internal Server Error';
-			} else if ($code == -32600) {
-				$message = 'Bad Request';
-				$status = 400;
-			} else if ($code == -32601) {
-
-				$override = $this->not_found();
-				if(!is_null($override)){
-					return $override;
-				}
-				$message = 'Not Found';
-				$status = 404;
-			}
-		}
-
-		if (!$this->suppress_http_status) {
-			header("Content-Type: application/json-rpc");
-			header("HTTP/1.1 " . $status . " " . $message);
-		}
-
-		//serialize PHP to preserve format of hashes if client requests it in headers
-		if ($this->php_serialize_response) {
-			$json_response = serialize($response);
-		} else {
-			$json_response = json_encode($response);
-		}
-
-		if (!empty($this->encryption_key)) {
-			$json_response = $this->encryptor->encrypt($json_response);
-		}
-
-		if ($this->gz_encode_level !== false) {
-			$json_response = gzencode($json_response, $this->gz_encode_level);
-		}
-
-		return $json_response;
 	}
 
 	/**
@@ -369,9 +195,176 @@ class sb_Controller_JSON_RPC2_Server extends sb_Controller {
 		return $html;
 	}
 
-	public function render() {
+	
+	/**
+	 * Parses the request
+	 * @param $json_request_str
+	 */
+	protected function get_response($json_request_str='') {
+
+		$response = new sb_JSON_RPC2_Response();
+
+		$request = null;
+		
+		if (empty($json_request_str)) {
+			
+			if(isset(Gateway::$cmd_options) && isset(Gateway::$cmd_options['json_request'])){
+				$json_request_str = Gateway::$cmd_options['json_request'];
+			} else if ($this->method == 'post' || $this->method == 'both') {
+				$json_request_str = file_get_contents("php://input");
+			}
+			
+			if(is_null($request) 
+					&& ($this->method == 'get' || $this->method == 'both')
+					&& (isset($this->request->get['method']) && isset($this->request->get['params']) && isset($this->request->get['id']))){
+				
+					$request = new sb_JSON_RPC2_Request();
+					$request->method = $this->request->get['method'];
+
+					$params = $this->request->get['params'];
+
+					if (!preg_match("~[\[\{]~", substr($params, 0, 1))) {
+
+						$params = base64_decode($params);
+					}
+
+					$request->params = json_decode($params);
+
+					$request->id = $this->request->get['id'];
+					$json_request_str = json_encode($request);
+			}
+		}
+		
+		if($json_request_str){
+			if (isset($this->encryption_key)) {
+				$json_request_str = $this->encryptor->decrypt($json_request_str);
+			}
+			
+		} 
+		$request = new sb_JSON_RPC2_Request($json_request_str);
+		
+		if (is_null($request)) {
+			$response->error = new sb_JSON_RPC2_Error(-32700, 'Parse Error', "Data Received: " . $json_request_str);
+		} else {
+			$response->id = $request->id;
+		}
+
+		if ($this->logger instanceof sb_Logger) {
+			$this->logger->add_log_types(Array('sb_json_rpc2_server'));
+			$this->logger->sb_json_rpc2_server("--> " . $json_request_str);
+		}
+		$servable = false;
+		
+		if (method_exists($this, $request->method)) {
+			$reflection = new ReflectionMethod($this, $request->method);
+
+			//check for phpdocs
+			$docs = $reflection->getDocComment();
+			$servable = false;
+			if (!empty($docs)) {
+				if (preg_match("~@servable (true|false)~", $docs, $match)) {
+					$servable = $match[1] == 'true' ? true : false;
+				}
+			}
+		}
+		//check for requested remote procedure
+		if ($servable) {
+
+			if (is_object($request->params)) {
+				$answer = call_user_func(Array($this, $request->method), $request->params);
+			} else if (is_array($request->params)) {
+				$answer = call_user_func_array(Array($this, $request->method), $request->params);
+			}
+
+			//if they return an error from the method call, return that
+			if ($answer instanceof sb_JSON_RPC2_Error) {
+				$response->error = $answer;
+			} else {
+				//otherwise return the answer
+				$response->result = $answer;
+			}
+		} else {
+
+			$response->error = new sb_JSON_RPC2_Error();
+			$response->error->code = -32601;
+			$response->error->message = "Procedure not found";
+		}
+
+		//remove unnecessary properties
+		if ($response->error instanceof sb_JSON_RPC2_Error) {
+
+			unset($response->result);
+			if (is_null($response->error->data)) {
+				unset($response->error->data);
+			}
+		} else {
+			unset($response->error);
+		}
+
+		//log the final response
+		if ($this->logger instanceof sb_Logger) {
+			$this->logger->sb_json_rpc2_server('<-- ' . json_encode($response));
+		}
+
+
+		return $response;
+	}
+	
+	/**
+	 * Serves data based on the json_request if set, otherwise based on Gateway::$cmd_options['json_request']
+	 * @param $json_request_str String This optional argument can be used for debugging the server.  A sb_JSON_RPC2_Request formatted JSON string e.g. {"method":"add","params":[1,2],"id":"abc123"}
+	 * @return string JSON encoded sb_JSON_RPC2_Response
+	 */
+	public function render($json_request_str='') {
 		if($this->on_before_render() !== false){
-			return $this->filter_output($this->handle());
+			
+			$response = $this->get_response($json_request_str);
+
+			$message = 'OK';
+			$status = 200;
+			//headers from spec here http://json-rpc.googlegroups.com/web/json-rpc-over-http.html
+			if (isset($response->error) && $response->error instanceof sb_JSON_RPC2_Error) {
+				$code = $response->error->code;
+
+				if (in_array($code, Array(-32700, -3260, -32603))
+					|| ($code <= -32000 && $code >= -32099)) {
+					$status = 500;
+					$message = 'Internal Server Error';
+				} else if ($code == -32600) {
+					$message = 'Bad Request';
+					$status = 400;
+				} else if ($code == -32601) {
+
+					$override = $this->not_found();
+					if(!is_null($override)){
+						return $override;
+					}
+					$message = 'Not Found';
+					$status = 404;
+				}
+			}
+
+			if (!$this->suppress_http_status) {
+				header("Content-Type: application/json-rpc");
+				header("HTTP/1.1 " . $status . " " . $message);
+			}
+
+			//serialize PHP to preserve format of hashes if client requests it in headers
+			if ($this->php_serialize_response) {
+				$json_response = serialize($response);
+			} else {
+				$json_response = json_encode($response);
+			}
+
+			if (!empty($this->encryption_key)) {
+				$json_response = $this->encryptor->encrypt($json_response);
+			}
+
+			if ($this->gz_encode_level !== false) {
+				$json_response = gzencode($json_response, $this->gz_encode_level);
+			}
+
+			return $this->filter_output($json_response);
 		}
 	}
 
@@ -385,5 +378,4 @@ class sb_Controller_JSON_RPC2_Server extends sb_Controller {
 	}
 
 }
-
 ?>
