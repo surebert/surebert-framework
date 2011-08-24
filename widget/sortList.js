25bc0549bcee7937baa955d049b76de80b80eeac
visco <visco@874840ac-8b26-453e-a097-40cf495e22eb>
2011-08-24 17:57:37 +0000

    updated sortList to work properly in IE and added several features including the ability to drag across the full LI

--- widget/sortList.js
+++ widget/sortList.js
@@ -6,107 +6,70 @@ sb.include('Element.prototype.cssTransition');
 @Version: 1.21 11/12/07 12/15/08
 @Description: used to make sortable lists
 @Example:
-myList = new sb.widget.sortList('#myList', '');
-
-myList.onSwitch = function(){
-	this.turnColor();
-};
-
-//this this of this is the list itself
-myList.onMouseUp = function(){
-	//document.title= this.dataOut();
-};
-
-//same time as onMouseUp but the this is the item itself
-myList.onItemDrop = function(){
-	this.turnColor2();
-	//document.title = this.order;
-};
-
-//the this of this is the item selected
-myList.onItemSelect = function(){
-	//document.title = this.order;
-};
+<style type="text/css">
+		#my_list li{
+			list-style-type:none;
+			border:1px solid #ACACAC;
+			background-color:white;
+		}
+		
+		sb_sortlist_handle{
+			background-image:url('http://webservicesdev.roswellpark.org/webui/icons/ns_arrow.png');
+			background-repeat:no-repeat;
+			margin-right:15px;
+			width:20px;
+			display:inline-block;
+		}
+	</style>
+<ul id="my_list">
+	<li id="a">Element 1</li>
+	<li id="b">Element 2</li>
+	<li id="c">Element 3</li>
+	<li id="d">Element 4</li>
+	<li id="e">Element 5</li>
+</ul>
+<script type="text/javascript">
+var list = new sb.widget.sortList({
+	list : '#my_list',
+	
+	onSwitch : function(){
+	},
+	onItemSelect : function(){
+		this.style.backgroundColor = '#DFDFDF';
+		//this.style.fontSize = (parseInt(this.getStyle('fontSize'), 10)+10)+'px';
+	},
+	onItemDrop :function(){
+		this.style.backgroundColor = '';
+		//this.spring();
+	}
+});
+</script>
 */
-//returns an element to its default position
-Element.prototype.getParent = function(){
-	if(this.parentNode){
-		return sb.$(this.parentNode);
-	}
-};
-
-Element.prototype.spring = function(){
-	var self = this;
-	self.cssTransition([{
-		prop : 'fontSize',
-		unit : 'px',
-		begin : 10,
-		change : +20,
-		type : 'outQuart',
-		onEnd : function(){
-			self.cssTransition([{
-				prop : 'fontSize',
-				unit : 'px',
-				type : 'inQuart',
-				begin : 30,
-				change : -10
-			}]).start();
-		}
-	}]).start();
-};
-
-Element.prototype.turnColor = function(){
-	var self = this;
-	if(this.style.backgroundColor !='#ffffff'){
-		this.style.backgroundColor='#ffffff';
-		window.setTimeout(function(){
-			self.style.backgroundColor='';
-		}, 300);
-	}
-};
-
-Element.prototype.turnColor2 = function(){
-	var self = this;
-	if(self.trans){
-		self.trans.stop();
-	}
-	self.trans = self.cssTransition([{
-		prop : 'backgroundColor',
-		type : 'outQuart',
-		begin : '#DFDFDF',
-		end : '#f59e38',
-		onEnd : function(){
-			self.cssTransition([{
-			prop : 'backgroundColor',
-			type : 'inQuart',
-			begin : '#f59e38',
-			end : '#DFDFDF'
-			}, 35]).start();
-		}
-	}, 35]).start();
-};
-
-sb.widget.sortList = function(id, innerHTML){
-	this.id = id;
-	this.list = sb.$(id);
-	this.listItems = sb.$(id+' li');
-	this.addButtons(innerHTML);
+sb.widget.sortList = function(params){
+	sb.objects.infuse(params, this);
+	this.list = sb.$(params.list);
+	this.listItems = this.list.$('li');
+	this.addButtons(params.handleHTML || '&nbsp;');
 	this.addEvents();
 };
 
 sb.widget.sortList.sorting = 0;
 sb.widget.sortList.mousemove = 0;
 sb.widget.sortList.draggedItem = 0;
-
+sb.widget.sortList.cursor = 'move';
 sb.widget.sortList.prototype = {
-	addButtons : function(innerHTML){
+	addButtons : function(handleHTML){
 		
 		this.listItems.forEach(function(li){
 		
 			var btn = new sb.element({
-				tag : 'button',
-				innerHTML : innerHTML || ' ',
-				title : 'drag to change list order'
+				tag : 'sb_sortlist_handle',
+				className : 'sb_sortlist_handle',
+				innerHTML : handleHTML,
+				title : 'drag to change list order',
+				styles : {
+					cursor: sb.widget.sortList.cursor
+				}
 				
 			}).appendToTop(li);
 		
@@ -119,14 +82,14 @@ sb.widget.sortList.prototype = {
 		var order =0;
 		sb.$(this.id+' li').forEach(function(li){
 			li.order =order;
-			li.firstChild.innerHTML = order;
+			//li.firstChild.innerHTML = order;
 			order++;
 		});
 	},
 	
 	dataOut : function(){
 		var order =[];
-		sb.$(this.id+' li').forEach(function(li, k){
+		this.list.$('li').forEach(function(li, k){
 			order[k] = li.id;
 		});
 		return order;
@@ -144,43 +107,50 @@ sb.widget.sortList.prototype = {
 				
 				var target = e.target;
 				
-				if(target.nodeName =='BUTTON' && target.parentNode && target.parentNode.nodeName =='LI'){
-					var currentItems = sb.$(self.id+ ' li');
-				
-					
-					sb.widget.sortList.draggedItem = target.getParent();
+				var handle = target.isOrHasParentOfClassName('sb_sortlist_handle');
+				if(handle){
+					var li = handle.getContaining('li');
+					var currentItems = self.list.$('li');
+					sb.widget.sortList.draggedItem = li;
 					
 					sb.widget.sortList.sorting=1;
 				
 					if(typeof self.onItemSelect == 'function'){
-						self.onItemSelect.call(sb.widget.sortList.draggedItem);
+						self.onItemSelect.call(li);
 					}
 					
 					var y=e.clientY;
 					
 					sb.widget.sortList.mousemove = sb.events.add(self.list, 'mouseover', function(e){
-						var dir=1;
+						self.dir=1;
 						if(y < e.clientY){
-							dir = 1;
+							self.dir = 1;
 						} else {
-							dir = -1;
+							self.dir = -1;
 						}
 						y = e.clientY;
 						sb.browser.removeSelection();
 						if(sb.widget.sortList.draggedItem){
 							var target = e.target;
 							
-							if(target.nodeName =='BUTTON' && target.parentNode && target.parentNode.nodeName =='LI' && target.parentNode !=this){
-								if(dir==1){
-									sb.widget.sortList.draggedItem.appendAfter(target.parentNode);
-								} else {
-									sb.widget.sortList.draggedItem.appendBefore(target.parentNode);
-								}
+							if(target.isWithin(self.list)){
 								
-								if(typeof self.onSwitch =='function'){
-									self.onSwitch.call(target.parentNode);
+								var li = target.nodeName == 'LI' ? target : target.getContaining('li');
+								
+								if(li && li != this){
+									
+									if(self.dir == 1){
+										sb.widget.sortList.draggedItem.appendAfter(li);
+									} else {
+										sb.widget.sortList.draggedItem.appendBefore(li);
+									}
+
+									if(typeof self.onSwitch =='function'){
+										self.onSwitch.call(li);
+									}
 								}
 							}
+							
 							
 						} else {
 							sb.events.remove(sb.widget.sortList.mousemove);
@@ -190,11 +160,10 @@ sb.widget.sortList.prototype = {
 			},
 			
 			mouseup : function(e){
-			
+				
 				if(sb.widget.sortList.draggedItem){
-					
+				
 					self.addOrder();
-					
 					if(typeof self.onItemDrop == 'function'){
 						self.onItemDrop.call(sb.widget.sortList.draggedItem);
 					}
