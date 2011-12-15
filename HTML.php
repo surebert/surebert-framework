377bb655253e41d9a67b7ff69922a89b9fbe0bd1
Paul Visco <paulsidekick@gmail.com>
2011-12-14 21:12:46 -0500

    added new sb_HTML::escape($mixed) and sb_HTML::unescape($mixed) which is used to recursively escape/unescape HTML string properties in object/array/string using htmlspecialchars and a reverse of it
    $arr = Array("fname" => "<b>paul</b>", "lname" => "<b>visco</b>");
    $arr = sb_HTML::escape($arr);
    echo $arr["name"];  // &lt;b&gt;paul...

---
+++ HTML.php
@@ -0,0 +1,60 @@
+<?php 
+class sb_HTML{
+	
+	/**
+	 * Recursively htmlspecialchars string properties of objects and arrays 
+	 * @param mixed $mixed The object or array to convert
+	 * @param int $quote_style ent quote style from htmlspecialchars
+	 * @param string $charset The charset from  htmlspecialchars
+	 * @return type 
+	 */
+	public static function escape($mixed, $quote_style = ENT_QUOTES, $charset = 'UTF-8') {
+		
+		if (is_string($mixed)) {
+			$mixed = htmlspecialchars($mixed, $quote_style, $charset);
+		} else if (is_object($mixed) || is_array($mixed)) {
+			foreach ($mixed as $k => &$v) {
+				if ($v) {
+					if (is_object($mixed)) {
+						$mixed->$k = self::escape($v, $quote_style, $charset);
+					} else {
+						$mixed[$k] = self::escape($v, $quote_style, $charset);
+					}
+				}
+			}
+		}
+
+		return $mixed;
+	}
+
+	/**
+	 * Recursively unhtmlspecialchars string properties of objects and arrays 
+	 * @param mixed $mixed The object or array to convert
+	 * @param int $quote_style ent quote style from htmlspecialchars
+	 * @param string $charset The charset from  htmlspecialchars
+	 * @return type 
+	 */
+	public static function unescape($mixed, $quote_style = ENT_QUOTES, $charset = 'UTF-8') {
+		
+		if (is_string($mixed)) {
+			$mixed = str_replace('&amp;', '&', $mixed);
+			$mixed = str_replace('&#039;', '\'', $mixed);
+			$mixed = str_replace('&quot;', '"', $mixed);
+			$mixed = str_replace('&lt;', '<', $mixed);
+			$mixed = str_replace('&gt;', '>', $mixed);
+		} else if (is_object($mixed) || is_array($mixed)) {
+			foreach ($mixed as $k => &$v) {
+				if ($v) {
+					if (is_object($mixed)) {
+						$mixed->$k = self::unescape($v);
+					} else {
+						$mixed[$k] = self::unescape($v);
+					}
+				}
+			}
+		}
+
+		return $mixed;
+	}
+}
+?>
