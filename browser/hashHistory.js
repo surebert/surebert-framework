fcf701f21636f736aaa01107fef7c3b51ca2c85c
Paul Visco <paulsidekick@gmail.com>
2012-02-11 01:05:35 -0500

    updated documentation

--- browser/hashHistory.js
+++ browser/hashHistory.js
@@ -23,10 +23,22 @@ sb.browser.hashHistory.init({
 	}
 });
 */
-sb.browser.hashHistory = {
 
+sb.browser.hashHistory = {
+	/**
+	 * The currentHash tag
+	 */
     currentHash : '',
+	/**
+	 * @Name: sb.browser.hashHistory.loading
+	 * @Description: Boolean Is hash url currently being loaded
+	 * 
+	 */
     loading : false,
+	/**
+	 * @Name sb.browser.hashHistory.saveHistoryToIframeForIE
+	 * @Description: Internal Use Only makes hash history work in IE7
+	 */
     saveHistoryToIframeForIE : function(hash){
 
         if(!this.iframe){
@@ -48,84 +60,120 @@ sb.browser.hashHistory = {
         doc.close();
     },
 
-
+	/**
+	 * @Name: sb.browser.hashHistory.updateHash
+	 * @Description: Changes the current hash
+	 */
     updateHash : function(hash){
         window.location.hash = hash;
     },
 
+	/**
+	 * @Name: sb.browser.hashHistory.processHashChange
+	 * @Description: Internal Use Only handles the hash change event
+	 */
 	processHashChange : function(){
 		var hash = window.location.hash.substring(1);
-		var self = this;
-		self.refreshing = false;
-
-		self.loading = true;
-		self.currentHash = hash;
-
-		if(self.currentHash == ''){
-
-			self.loading = false;
+		
+		this.refreshing = false;
+		this.currentHash = hash;
+		
+		if(this.currentHash == ''){
+			this.loading = false;
 			return false;
 		}
-
-		if(typeof self.onLoading == 'function'){
-			self.onLoading();
-		}
-
+		
 		//IE crap to make history work
 		if(sb.browser.agent == 'ie'){
-			self.saveHistoryToIframeForIE(self.currentHash);
+			this.saveHistoryToIframeForIE(this.currentHash);
 		}
-
-		self.onHashChange(self.currentHash);
+		
+		this.loading = true;
+		
+		if(this.onLoading(hash) === false){
+			this.loading = false;
+			return false;
+		}
+		this.onHashChange(this.currentHash);
 
 	},
-
+	
+	/**
+	 * @Name: sb.browser.hashHistory.onHashChange
+	 * @Description: Internal Use Only fires off ajax request based on hash
+	 */
 	onHashChange : function(hash){
 		var self = this;
+		
+		hash = hash.replace('+', '%2B');
 		var aj = new sb.ajax({
 			url : hash,
-			onHeaders : function(status){
+			method : this.method || 'post',
+			onHeaders : function(status, statusText){
 
 				if(status != 200){
 					self.loading = false;
 					self.onPageNotFound();
 				}
+				sb.ajax.prototype.onHeaders.call(this, status, statusText);
 			},
 			onResponse : function(r){
 
 				if(typeof self.onLoaded == 'function'){
-					self.onLoaded(r);
+					self.onLoaded(r, hash);
 				}
 
 				sb.browser.hashHistory.loading = false;
 			}
 		}).fetch();
 	},
+	
+	/**
+	 * @Name: sb.browser.hashHistory.onLoading
+	 * @Description: Fires when the hash changes
+	 * @Param: hash string the hash
+	 */
+	onLoading : function(hash){},
+	
+	/**
+	 * @Name: sb.browser.hashHistory.onLoaded
+	 * @Description: Fires when the URL loads
+	 * @Param: content string the content from the URL
+	 * @Param: hash string the hash
+	 */
+	onLoaded : function(content, hash){},
+	
+	/**
+	 * @Name: sb.browser.hashHistory.onPageNotFound
+	 * @Description: Fires when the URL returns a 404
+	 */
+	onPageNotFound : function(){},
 
-    startTimer : function(){
-        var self = this;
-        window.setInterval(function(){
-			var hash = window.location.hash.substring(1);
-            if(!self.loading  && self.refreshing || (hash != self.currentHash) ){
-				self.processHashChange();
-            }
-        }, 100);
-    },
-
+	/**
+	 * @Name: sb.browser.hashHistory.reload
+	 * @Description: sets the process event to allow reload
+	 */
 	reload : function(){
 		this.refreshing = true;
 	},
 
+	/**
+	 * @Name: sb.browser.hashHistory.init
+	 * @Description: sets a hashchange event or starts a timer if the hashchange event is not available
+	 */
     init : function(o){
 		var self = this;
-        this.onLoading = o.onLoading;
-        this.onLoaded = o.onLoaded;
-		this.onPageNotFound = o.onPageNotFound || function(){};
-
+		sb.objects.infuse(o, this);
 		if(window.onhashchange){
 			sb.events.add(window, 'hashchange', function(){self.processHashChange});
 		} else {
-			this.startTimer();
+			var self = this;
+			window.setInterval(function(){
+				var hash = window.location.hash.substring(1);
+				if(!self.loading  && self.refreshing || (hash != self.currentHash) ){
+					self.processHashChange();
+				}
+			}, 100);
 		}
         
 
