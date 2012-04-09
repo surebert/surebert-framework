c3f336b4e7925212d4a12df9313911d481cebbf1
Paul Visco <paulsidekick@gmail.com>
2012-04-09 18:58:16 -0400

    removing toolkit files after toolkit and framework repos getting merged on github

--- colors/hex2dec.js
+++
@@ -1,9 +0,0 @@
-/**
-@Name: sb.colors.hex2dec
-@Description: coverts hex values to decimal
-@Example: 
-var hex = sb.colors.hex2dec('AC');
-*/
-sb.colors.hex2dec = function(hex){
-	return parseInt(hex,16);
-};
