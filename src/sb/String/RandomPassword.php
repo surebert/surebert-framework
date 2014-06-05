ecc1c76ec9f616fa6c8b0343f60c0b27dab26925
Visco, Paul <paul.visco@roswellpark.org>
2014-06-04 20:17:41 -0400

    *moved \sb\Strings\RandomPassword to \sb\Password\Random, kept placeholder class for backwards compat

--- src/sb/String/RandomPassword.php
+++ src/sb/String/RandomPassword.php
@@ -4,119 +4,14 @@
  * Generates a random lower/upper letter + numbers password of a specified length
  * Makes sure at least one char is cpaitalize
  *
- * Makes sure at least one
+ * Makes sure at least one symbol is used
+ * 
+ * @deprecated use \sb\Password\Random() instead, this is just kept for 
+ * backwards compat purposes
  *
  * @author paul.visco@roswellpark.org
  * @package String
  */
 namespace sb\String;
 
-class RandomPassword
-{
-
-    /**
-     * The length of password to generate
-     * @var integer
-     */
-    protected $length = 6;
-
-    /**
-     * The password that is generated
-     * @var string
-     */
-    protected $password = '';
-
-    /**
-     * Create a password of a certain length
-     *
-     * <code>
-     * echo new \sb\String\RandomPassword(6);
-     * </code>
-     *
-     * @param integer $length The length of the password to generate
-     * @param boolean $mixed_case Determines if mixed case is used
-     */
-    public function __construct($length, $mixed_case = true)
-    {
-
-        if (preg_match("~^\d+$~", $length)) {
-            $this->length = $length;
-        } else {
-            throw(new \Exception("Length must an integer"));
-        }
-
-        $this->generate();
-
-        if ($mixed_case) {
-            $this->randomlyCapitalize();
-        }
-    }
-
-    /**
-     * Generates the password randomly
-     */
-    protected function generate()
-    {
-
-        $this->password = '';
-
-        $chars = range('a', 'z');
-
-        $symbols = Array('*', '&', '^', '%', '$', '#', '@', '!');
-
-        $chars = array_merge($chars, $symbols);
-
-        //get rid of l and o as they can be confused with 1 and 0
-        unset($chars[11]);
-        unset($chars[14]);
-
-        $chars = array_values($chars);
-
-        $chars_length = count($chars);
-
-        foreach (range(1, $this->length) as $char) {
-
-            $char = '';
-
-            $integer = rand(2, 9);
-            $new_char = $chars[rand(0, $chars_length - 1)];
-            if (strstr($this->password, $new_char)) {
-                $char = $new_char;
-            } elseif (strstr($this->password, $integer)) {
-                $char = $integer;
-            } else {
-                $char = rand(0, 1) ? $new_char : $integer;
-            }
-
-            $this->password .= $char;
-        }
-
-        $this->password = substr_replace($this->password, array_rand($symbols), rand(0, strlen($this->password)), 1);
-    }
-
-    /**
-     * Randomly capitalize one of the letters in the password
-     */
-    protected function randomlyCapitalize()
-    {
-        $chars = str_split($this->password);
-        $letter_positions = Array();
-        foreach ($chars as $key => $char) {
-            if (!is_numeric($char)) {
-                $letter_positions[] = $key;
-            }
-        }
-
-
-        $random_letter = $letter_positions[array_rand($letter_positions)];
-
-        $chars[$random_letter] = ucwords($chars[$random_letter]);
-
-        $this->password = implode('', $chars);
-    }
-
-    public function __toString()
-    {
-        return $this->password;
-    }
-}
+class RandomPassword extends \sb\Password\Random{}
