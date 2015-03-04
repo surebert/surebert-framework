b0fe72b7834d01d99a1d3e1f16a3e91f9ab91125
Joe Herzig <0891.joe@gmail.com>
2015-03-04 02:56:50 -0500

    Updated PDO Logger and documentation from SBF2 style to SBF3

--- src/sb/PDO/Logger.php
+++ src/sb/PDO/Logger.php
@@ -13,7 +13,7 @@ class Logger extends Debugger
 {
 
     /**
-     * An instance of \sb\Logger_Base used to do the logging
+     * An instance of \sb\Logger\Base used to do the logging
      * @var \sb\Logger_Base
      */
     private $logger = null;
@@ -32,8 +32,8 @@ class Logger extends Debugger
      * @param string $pass Password for connection if required
      *
      * <code>
-     * $db=new  \sb\PDO_Logger("mysql:dbname=xxx;host=xxx", 'username', 'pass');
-     * $db=new \sb\PDO_Logger("sqlite:myfile.db3');
+     * $db=new  \sb\PDO\Logger("mysql:dbname=xxx;host=xxx", 'username', 'pass');
+     * $db=new \sb\PDO\Logger("sqlite:myfile.db3');
      * $db->setLogger(App::$logger);
      * </code>
      *
@@ -43,7 +43,7 @@ class Logger extends Debugger
 
         $this->log_str = str_replace(Array(":", ";", "=", "."), "_", $connection);
         parent::__construct($connection, $user, $pass);
-        $this->setAttribute(PDO::ATTR_STATEMENT_CLASS, Array('sb_PDO_Statement_Logger', Array($this)));
+        $this->setAttribute(\PDO::ATTR_STATEMENT_CLASS, Array('\sb\PDO\Statement\Logger', Array($this)));
     }
 
     /**
